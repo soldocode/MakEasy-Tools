d2cd6a4cbf8988dd2191c26a7733ba0a80a0dc4b
Riccardo Soldini <soldocode@users.noreply.github.com>
2020-12-01 19:38:57 +0100

    FCTreeSheet class updated

--- MeFreeCADClasses.py
+++ MeFreeCADClasses.py
@@ -36,8 +36,9 @@ class FCTreeSheet(object):
 				f1=obj.Faces[idf1]
 				f2=obj.Faces[idf2]
 				dist=round(f1.distToShape(f2)[0],1)
+				print('thk:',dist,' ->',idf1,'-',idf2)
 				if dist==obj.Thk:
-					#print('thk:',dist,' ->',idf1,'-',idf2)
+					print('couple matched')
 					couples.append([idf1,idf2])
 			id_key+=1
 
@@ -55,6 +56,7 @@ class FCTreeSheet(object):
 		s_up=self.FCObject.Faces[face_up].Surface
 		s_down=self.FCObject.Faces[face_down].Surface
 		new_branch=Branch()
+		new_branch.Joints={}
 		new_branch.Class=s_up.__class__.__name__
 		new_branch.FaceUp=face_up
 		new_branch.FaceDown=face_down
@@ -65,10 +67,10 @@ class FCTreeSheet(object):
 			new_branch.Radius=(s_up.Radius+s_down.Radius)/2
 		return new_branch
 
-	def get_branches(self,face_up_from,map,couples):
+	def get_branches(self,face_up_from,map,couples,bb={}):
 		next_calls=[]
 		#branches=[]
-		bb={}
+		#bb={}
 		face_down=None
 		for c in couples:
 			if face_up_from==c[0]:
@@ -92,38 +94,33 @@ class FCTreeSheet(object):
 					for geo_down_from in links_down:
 						face_down_to=links_down[geo_down_from][0]
 						if face_down_to in p:
+							print (face_down_to,' found in',p,' with geo id ',geo_down_from)
 							geo_up_to=links_up[geo_up_from][1]
 							geo_down_to=links_down[geo_down_from][1]
 							if not face_up_from in bb:
-								#branch_from=Branch()
-								#branch_from.FaceUp=face_up_from
-								#branch_from.FaceDown=face_down_from
-								#branch_from.Class=self.FCObject.Faces[face_up_from].Surface.__class__.__name__
+								print ('... new branch with id ',face_up_from)
 								bb[face_up_from]=self.create_new_branch(face_up_from,
 													          			face_up_from,
 															   			face_down_from)
 							br_from=bb[face_up_from]
 							if not face_up_to in bb:
-								#branch_to=Branch()
-								#branch_to.FaceUp=face_up_to
-								#branch_to.FaceDown=face_down_to
-								#branch_to.Class=self.FCObject.Faces[face_up_to].Surface.__class__.__name__
+								print ('... new branch with id ',face_up_to)
 								bb[face_up_to]=self.create_new_branch(face_up_to,
 													          		  face_up_to,
 															   		  face_down_to)
 							br_to=bb[face_up_to]
-							j1=br_to.Joints[geo_up_from]=Joint()
+							print('previous joints...',br_to.Joints)
+							j1=br_from.Joints[geo_up_from]=Joint()
 							j1.FromBranch=br_from
 							j1.ToBranch=br_to
 							j1.JoinUp=[geo_up_from,face_up_to,geo_up_to]
 							j1.JoinDown=[geo_down_from,face_down_to,geo_down_to]
-							j2=br_from.Joints[geo_up_to]=Joint()
+							j2=br_to.Joints[geo_up_to]=Joint()
 							j2.FromBranch=br_to
 							j2.ToBranch=br_from
 							j2.JoinUp=[geo_up_to,face_up_from,geo_up_from]
 							j2.JoinDown=[geo_down_to,face_down_from,geo_down_from]
 
-							print (face_down_to,' found in',p,' with geo id ',geo_down_from)
 							#branches.append(dict(face_up_from=face_up_from,
 							#					 geo_up_from=geo_up_from,
 							#					 face_up_to=face_up_to,
@@ -139,7 +136,7 @@ class FCTreeSheet(object):
 		c.remove(couple)
 		for n in next_calls:
 			#branches+=self.get_branches(n,map,c)
-			bb.update(self.get_branches(n,map,c))
+			bb.update(self.get_branches(n,map,c,bb))
 		return bb
 
 class Branch(object):
